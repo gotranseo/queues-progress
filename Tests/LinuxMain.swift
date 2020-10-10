96417387978751bb8b5fb5721146ce07e890f085
Jimmy McDermott <mcdappdev@gmail.com>
2020-10-10 14:59:30 -0400

    Remove Tests folder

--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import queues_progressTests
-
-var tests = [XCTestCaseEntry]()
-tests += queues_progressTests.allTests()
-XCTMain(tests)
--- Tests/queues-progressTests/XCTestManifests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-#if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(queues_progressTests.allTests),
-    ]
-}
-#endif
